8dca2dea9e60dd6a5777466ced925664b4038625
paulobaldovi <baldovipaulo@gmail.com>
2022-06-13 06:45:56 -1000

    used Bootstrap's forms

--- events/forms.py
+++ events/forms.py
@@ -11,3 +11,13 @@ class EventForm(forms.ModelForm):
         widgets = {
             'date': DateInput(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(EventForm, self).__init__(*args, **kwargs)
+        self.fields['description'].widget.attrs = {
+            'class': 'form-control mb-3',
+            'placeholder': 'description',
+        }
+        self.fields['date'].widget.attrs = {
+            'class': 'form-control mb-3',
+        }
