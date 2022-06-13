c7c065387d44eee1836f6785927eb1cc6d35e259
paulobaldovi <baldovipaulo@gmail.com>
2022-06-13 05:58:22 -1000

    updated to display difference in days between target date and today

--- events/models.py
+++ events/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Event(models.Model):
     description = models.CharField(max_length=200)
     date = models.DateTimeField('date')
+
+    def __str__(self):
+        return "There are " + str(self.date_diff) + " days until " + self.description
+
+    @property
+    def date_diff(self):
+        today = timezone.now()
+        return (self.date - today).days
