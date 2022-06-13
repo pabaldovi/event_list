6e277b3f31c03befb8ac8d3370086f4e9a56482b
paulobaldovi <baldovipaulo@gmail.com>
2022-06-13 05:50:36 -1000

    updated for date input

--- events/forms.py
+++ events/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from .models import Event
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['description']
-
+        fields = ['description', 'date']
+        widgets = {
+            'date': DateInput(),
+        }
