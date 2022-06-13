2e174d45b43aa189066c60853712a34c4bc7575f
paulobaldovi <baldovipaulo@gmail.com>
2022-06-13 05:05:28 -1000

    created create view

--- events/views.py
+++ events/views.py
@@ -1,6 +1,14 @@
 from django.views import generic
+from django.urls import reverse_lazy
 from .models import Event
+from .forms import EventForm
 
 class IndexView(generic.ListView):
     template_name = 'events/index.html'
     model = Event
+
+class CreateView(generic.edit.CreateView):
+    template_name = 'events/create.html'
+    model = Event
+    form_class = EventForm
+    success_url = reverse_lazy('events:index')
