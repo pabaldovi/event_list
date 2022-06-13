f2e5267442629649a09bbb83b5edbcd8b637cf49
paulobaldovi <baldovipaulo@gmail.com>
2022-06-13 04:41:53 -1000

    create index view

--- events/views.py
+++ events/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Event
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'events/index.html'
+    model = Event
