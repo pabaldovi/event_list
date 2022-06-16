9dcff2df79de0fbac8e5d555963a0c81964b98f0
paulobaldovi <baldovipaulo@gmail.com>
2022-06-15 14:44:02 -1000

    changed database from sqlite to postgres

--- scripts/setup_app/migrate_database.sh
+++ scripts/setup_app/migrate_database.sh
@@ -2,4 +2,5 @@
 
 # This script migrates the database
 
+python manage.py makemigrations events
 python manage.py migrate
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python manage.py runserver 0.0.0.0:8000 # Docker requires 0.0.0.0
+./scripts/setup_app/add_seed_data.sh
+python manage.py runserver 0.0.0.0:"$PORT" # Docker requires 0.0.0.0
