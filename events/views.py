e4dbe100606179fd6e050b60e425781d1b0f0454
paulobaldovi <baldovipaulo@gmail.com>
2022-06-27 03:48:14 -1000

    added login requirement for views

--- events/views.py
+++ events/views.py
@@ -2,28 +2,34 @@ from django.views import generic
 from django.urls import reverse_lazy
 from .models import Event
 from .forms import EventForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class IndexView(generic.ListView):
+class IndexView(LoginRequiredMixin, generic.ListView):
+    login_url = '/login/'
     template_name = 'events/index.html'
     model = Event
 
-class CreateView(generic.edit.CreateView):
+class CreateView(LoginRequiredMixin, generic.edit.CreateView):
+    login_url = '/login/'
     template_name = 'events/create.html'
     model = Event
     form_class = EventForm
     success_url = reverse_lazy('events:index')
 
-class DetailView(generic.DetailView):
+class DetailView(LoginRequiredMixin, generic.DetailView):
+    login_url = '/login/'
     template_name = 'events/detail.html'
     model = Event
 
-class UpdateView(generic.edit.UpdateView):
+class UpdateView(LoginRequiredMixin, generic.edit.UpdateView):
+    login_url = '/login/'
     template_name = 'events/update.html'
     model = Event
     form_class = EventForm
     success_url = reverse_lazy('events:index')
 
-class DeleteView(generic.edit.DeleteView):
+class DeleteView(LoginRequiredMixin, generic.edit.DeleteView):
+    login_url = '/login/'
     template_name = 'events/delete.html'
     model = Event
     success_url = reverse_lazy('events:index')
