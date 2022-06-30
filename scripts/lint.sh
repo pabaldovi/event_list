fd9d8b2e042aaa6d9d56029b763cc32198c62e48
paulobaldovi <baldovipaulo@gmail.com>
2022-06-29 17:08:50 -1000

    added pylint

---
+++ scripts/lint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pylint events
+pylint users
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+./scripts/lint.sh
 ./scripts/setup_app/add_seed_data.sh
 python manage.py runserver 0.0.0.0:"$PORT" # Docker requires 0.0.0.0
