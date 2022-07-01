c5f0256201d220eff30ba5ff22db1bd6ce7ca2e0
paulobaldovi <baldovipaulo@gmail.com>
2022-06-30 23:20:23 -1000

    added class docstring

--- events/apps.py
+++ events/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class EventsConfig(AppConfig):
+    """Default config for Events."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'events'
--- users/apps.py
+++ users/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """Default config for Users."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
